b42a3ed1b1b4dfb001407bd645824f2556347b0a
yuliadrozdova <drozdova.exceedteam@gmail.com>
2021-09-17 14:48:58 +0300

    code refactoring done

--- Work/Module3/NodeJS_To-do_List/app.js
+++ Work/Module3/NodeJS_To-do_List/app.js
@@ -14,61 +14,6 @@ mongoose.connect(uri);
 //////////app.use(bodyParser.json());
 app.use("/", apiRoutes);
 
-// app.get('/allTasks', (req,res) => {
-//     Task.find().then(result => {
-//         res.send({data: result});
-//     });
-// });
-//
-// app.post('/createTask', (req,res) => {
-//     const task = new Task(req.body);
-//     task.save().then(result => {
-//         res.send('Task created');
-//     })
-// });
-//
-// app.delete('/deleteTask', (req,res) => {
-//     Task.deleteOne({text: "First task"}).then(result => {
-//         res.send('Task deleted');
-//     })
-// });
-//
-// app.put('/updateTask', (req,res) => {
-//     Task.updateOne({text: 'First task'}, {text: 'First task-2'}).then(result => {
-//         res.send('Task updating');
-//     })
-// });
-
 app.listen(8080, () => {
     console.log('Example app listening on port 8080!')
-});
-
-//*************************************************************
-// app.get("/", (req, res) => {
-//    const task = new Task({
-//        text: 'Second task',
-//        isCheck: false
-//    });
-//    task.save().then(result => {
-//        res.send(result);
-//    }).catch(err => console.log(err))
-//
-// });
-//
-// app.get("/paramRequest", (req, res) => {
-//     Task.find().then(result => {
-//         res.send({data: result});
-//     })
-// });
-//
-// app.post("/", (req, res) => {
-//     res.send('post /')
-// });
-//
-// app.put("/", (req, res) => {
-//     res.send('put /')
-// });
-//
-// app.delete("/", (req, res) => {
-//     res.send('delete /')
-// });
+})
